12604e5fb770a120c3d787969b8bdef24cf39f9e
jlangley3 <jlangleyiii@aol.com>
2020-08-22 15:53:47 -0400

    cleaning up HomePage

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -40,26 +40,27 @@ class Homepage extends React.Component{
   addEventBtn = () => {
     return (
       <Modal trigger={<Button onClick={() => this.handleOpen('addEventModal')}
-      className="fluid ui button"
-      inverted
-      
-      size="massive"
-      color='green'><Icon name="add square"/>Create Gift List</Button>} 
-        open={this.state.addEventModal}
-        onClose={() => this.handleClose('addEventModal')}
-        closeIcon
-      centered={false}>
-      <Modal.Header as="h4">
-      <Icon name='add circle' size='large' color='green' />
-        Add a New Event Gift List
-      </Modal.Header>
-      <Modal.Content>
-      <NewEventForm 
-      title={"Add Event Form"} 
-      handleClose={() => this.handleClose('addEventModal')}/> 
-      </Modal.Content>
+          className="fluid ui button"
+          inverted
+          size="massive"
+          color='green'>
+        <Icon name="add square"/>Create Gift List</Button>} 
+            open={this.state.addEventModal}
+            onClose={() => this.handleClose('addEventModal')}
+            closeIcon
+            centered={false}>
+        <Modal.Header as="h4">
+        <Icon name='add circle' size='large' color='green' />
+            Add a New Event Gift List
+        </Modal.Header>
+        <Modal.Content>
+        <NewEventForm 
+            title={"Add Event Form"} 
+            handleClose={() => this.handleClose('addEventModal')}/> 
+        </Modal.Content>
     </Modal>
     )
+
   }
  randomColor = () => {
   Math.floor(Math.random()*16777215).toString(16);
@@ -71,55 +72,49 @@ render(){
 
     return (
         <div className='wrapper'>
-                <Header as='h2' color="red" dividing>
-                  <Icon name='gift' color="green" />
-                    <Header.Content  className="ui red header">Your Gift Lists</Header.Content>
-                      </Header>
-         <Grid columns="equal"  stackable>
-            <Grid.Row>
-              {/* <Grid.Column >
-                          </Grid.Column> */}
-                            <Grid.Column>
-                                  <Input
-                                        icon='search'
-                                        type='text'
-                                        size='huge'
-                                        value={this.props.searchTerm}
-                                        onChange={(event) => this.props.updateSearchTerm(event.target.value)}
-                                        name='filter'
-                                        placeholder='Search for a list'>
-                                    </Input>
-                                  </Grid.Column>
-                                  <Grid.Column>
-                              <Segment compact>
-                                <Label>Upcoming Events</Label>
-                                    <Checkbox 
-                                        toggle
-                                        className="togglehome"
-                                        color="green"
-                                        checked={this.state.checked}
-                                        onChange={this.handleToggle}
-                                        // label='Show all Lists'
-                                        />
+            <Header as='h2' color="red" dividing>
+              <Icon name='gift' color="green" />
+                <Header.Content  className="ui red header">Your Gift Lists</Header.Content>
+            </Header>
+            <Grid columns="equal"  stackable>
+                <Grid.Row>
+                    <Grid.Column>
+                          <Input
+                                icon='search'
+                                type='text'
+                                size='huge'
+                                value={this.props.searchTerm}
+                                onChange={(event) => this.props.updateSearchTerm(event.target.value)}
+                                name='filter'
+                                placeholder='Search for a list'>
+                            </Input>
+                      </Grid.Column>
+                      <Grid.Column>
+                          <Segment compact>
+                            <Label>Upcoming Events</Label>
+                                <Checkbox 
+                                    toggle
+                                    className="togglehome"
+                                    color="green"
+                                    checked={this.state.checked}
+                                    onChange={this.handleToggle}
+                                    // label='Show all Lists'
+                                    />
                               <Label>Show All Lists</Label>
-                                  </Segment>
+                            </Segment>
                         </Grid.Column>
                         <Grid.Column >
-                        {this.addEventBtn()}  
-                    </Grid.Column>
-              </Grid.Row>
-          </Grid>
-
-                                    <p />
-                                      {this.props.events.length === 0 ?
-                                      <div>
-                                        <NoEvents />
-                                        
-                                      </div>
-        
-                      :
-                                      <div className='ui four stackable cards'>
-     
+                            {this.addEventBtn()}  
+                        </Grid.Column>
+                  </Grid.Row>
+              </Grid>
+                <p />
+                  {this.props.events.length === 0 ?
+                  <div>
+                    <NoEvents />
+                  </div>
+                  :
+                  <div className='ui four stackable cards'>
                      { this.state.checked ?
                       this.filteredEvents().sort((a, b) => {
                         const createdA = new Date(a.start_date)
@@ -160,22 +155,22 @@ render(){
                     </div> : null}
 
                     <Modal
-                          open={this.state.editEventModal} 
-                          closeIcon
-                          centered={true}
-                          onClose={() => {
-                            // this.toggleEditForm()
-                            this.handleClose('editEventModal')}
-                          }
-                          size='large'
-                          >
-                          <Segment>
-                              <EventShow
-                                event={this.state.thisEvent}
-                                handleClose={() => this.handleClose('editEventModal')}
-                              />
-                          </Segment>
-                          </Modal>
+                        open={this.state.editEventModal} 
+                        closeIcon
+                        centered={true}
+                        onClose={() => {
+                          // this.toggleEditForm()
+                          this.handleClose('editEventModal')}
+                        }
+                        size='large'
+                        >
+                        <Segment>
+                            <EventShow
+                              event={this.state.thisEvent}
+                              handleClose={() => this.handleClose('editEventModal')}
+                            />
+                        </Segment>
+                      </Modal>
 
         </div>
       )
